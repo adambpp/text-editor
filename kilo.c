b7f892656487448291f1920ef7a4e438deed59b3
Adam <adambpp@gmail.com>
2024-07-27 17:01:39 -0600

    added raw mode for the terminal

--- kilo.c
+++ kilo.c
@@ -1,3 +1,30 @@
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+// restore the terminal to its original state
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+// raw mode (your inputs will no longer be visible)
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
 int main() {
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
