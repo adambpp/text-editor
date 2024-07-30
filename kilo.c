fe4d20a7f70c6df4c3616f028a3c0202fcee8168
Adam <adambpp@gmail.com>
2024-07-29 19:37:48 -0600

    added getWindowSize function

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 /*** defines ***/
@@ -12,8 +13,11 @@
 #define CTRL_KEY(k) (k & 0x1f)
 
 /*** data ***/
+struct editorConfig {
+    struct termios orig_termios;
+};
 
-struct termios orig_termios;
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -35,7 +39,7 @@ void die(const char *s) {
 * Restores the terminal to its original state
 */
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
@@ -43,10 +47,10 @@ void disableRawMode() {
 * raw mode (your inputs will no longer be visible)
 */
 void enableRawMode() {
-   if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+   if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     // disable various input/output/local flag modes 
     // via bitwise OR, NOT and bitwise ANDing
@@ -73,6 +77,21 @@ char editorReadKey() {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
     return c;
+}
+
+/*
+* Gets the terminal window size via ioctl (Input/Output/Control)
+*/
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 /*** output ***/
