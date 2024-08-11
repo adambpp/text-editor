2702c7efaa1f7f4b29f599268d5efe1e1e69500f
Adam <adambpp@gmail.com>
2024-08-11 15:35:11 -0600

    Starting to implement file reading and output functionality

--- kilo.c
+++ kilo.c
@@ -1,5 +1,10 @@
 /*** includes  ***/
 
+// These may be neccesary in order for getline() to work
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -214,17 +219,30 @@ int getWindowSize(int *rows, int *cols) {
 /*** file i/o ***/
 
 /*
-This function will be for opening and reading a file from disk (doesn't do that yet though)
+This function purpose is to open and read a file from disk
 */
-void editorOpen() {
-    char *line = "Hello, world!";
-    ssize_t linelen = 13;
+void editorOpen(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die ("fopen");
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    // empty values for getline to fill up
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    // feed the values into getline() which will then return the length of the line read
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != -1) {
+        while (line > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
+            linelen--;
+            }
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+    free(line);
+    fclose(fp);
 }
 
 
@@ -425,10 +443,12 @@ void initEditor() {
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
-    editorOpen();
+    if (argc >= 2) {
+        editorOpen(argv[1]);
+    }
 
     while (1) {
         editorRefreshScreen();
